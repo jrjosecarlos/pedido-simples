9b23b381e9e8f5f653a80d93806fc4b550b8d4b7
jrjosecarlos <jr_josecarlos@yahoo.com.br>
2020-11-28 19:10:58 -0300

    pedido-simples: commit inicial

---
+++ src/main/java/br/org/casa/pedidosimples/PedidosimplesApplication.java
@@ -0,0 +1,13 @@
+package br.org.casa.pedidosimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/casa/pedidosimples/PedidosimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.casa.pedidosimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
