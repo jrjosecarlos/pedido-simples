e9ed5a6c4b5965260f0b65d5b4e77f53eb1695e9
José Carlos da Conceição Junior <jr_josecarlos@yahoo.com.br>
2020-11-29 13:22:14 -0300

    feature/cadastro-pedido: adicionado AttributeConverter para TipoItemVenda

---
+++ src/main/java/br/org/casa/pedidosimples/model/TipoItemVendaAttributeConverter.java
@@ -0,0 +1,29 @@
+/**
+ *
+ */
+package br.org.casa.pedidosimples.model;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/**
+ * Um {@link AttributeConverter} para persistir campos da classe {@link TipoItemVenda}
+ * utilizando o valor correspondente ao elemento.
+ *
+ * @author jrjosecarlos
+ *
+ */
+@Converter(autoApply = true)
+public class TipoItemVendaAttributeConverter implements AttributeConverter<TipoItemVenda, String> {
+
+	@Override
+	public String convertToDatabaseColumn(TipoItemVenda tipoItemVenda) {
+		return tipoItemVenda == null ? null : tipoItemVenda.getValor();
+	}
+
+	@Override
+	public TipoItemVenda convertToEntityAttribute(String dbData) {
+		return dbData == null ? null : TipoItemVenda.fromValor(dbData);
+	}
+
+}
