8099d7314190be796a4253b51eb291a8aad3edd6
José Carlos da Conceição Junior <jr_josecarlos@yahoo.com.br>
2020-11-29 19:31:43 -0300

    feature/cadastro-pedido: atualizados métodos de ItemVendaController para retornarem objetos ResponseEntity, ao invés das entidades puras.
    A maior parte deles foi atualizada para retornar também os códigos de retorno HTTP corretos, dependendo do caso
    (pendente atualizar PUT e DELETE com erro 404).

--- src/main/java/br/org/casa/pedidosimples/controller/ItemVendaController.java
+++ src/main/java/br/org/casa/pedidosimples/controller/ItemVendaController.java
@@ -4,9 +4,9 @@
 package br.org.casa.pedidosimples.controller;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,11 +14,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.org.casa.pedidosimples.model.ItemVenda;
 import br.org.casa.pedidosimples.repository.ItemVendaRepository;
 
 /**
+ * Controller para exposição dos serviços REST relacionados a {@link ItemVenda}.
  *
  * @author jrjosecarlos
  *
@@ -33,27 +35,43 @@ public class ItemVendaController {
 	}
 
 	@GetMapping("/itens-venda")
-	List<ItemVenda> listarTodosItensVenda() {
-		return repository.findAll();
+	ResponseEntity<List<ItemVenda>> listarTodosItensVenda() {
+		return ResponseEntity.ok(repository.findAll());
 	}
 
 	@GetMapping("/item-venda/{uuid}")
-	Optional<ItemVenda> buscarItemVendaPorId(@PathVariable("uuid") UUID uuid) {
-		return repository.findById(uuid);
+	ResponseEntity<ItemVenda> buscarItemVendaPorId(@PathVariable("uuid") UUID uuid) {
+		return ResponseEntity.of(repository.findById(uuid));
 	}
 
 	@PostMapping("/item-venda")
-	ItemVenda incluirItemVenda(@RequestBody ItemVenda itemVenda) {
-		return repository.save(itemVenda);
+	ResponseEntity<ItemVenda> incluirItemVenda(@RequestBody ItemVenda itemVenda) {
+		ItemVenda criado = repository.save(itemVenda);
+
+		return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{uuid}")
+					.buildAndExpand(criado.getId())
+					.toUri()
+				)
+				.body(criado);
 	}
 
-	@PutMapping("/item-venda")
-	ItemVenda atualizarItemVenda(@RequestBody ItemVenda itemVenda) {
-		return repository.save(itemVenda);
+	@PutMapping("/item-venda/{uuid}")
+	ResponseEntity<ItemVenda> atualizarItemVenda(@RequestBody ItemVenda itemVenda, @PathVariable("uuid") UUID uuid) {
+		ItemVenda atualizado = repository.findById(uuid)
+				.map(iv -> {
+					itemVenda.setId(uuid);
+					return repository.save(itemVenda);
+				}).orElseThrow( () -> new IllegalArgumentException("Entidade não encontrada"));
+
+		return ResponseEntity.ok(atualizado);
 	}
 
 	@DeleteMapping("/item-venda/{uuid}")
-	void excluirItemVenda(@PathVariable("uuid") UUID uuid) {
-		repository.deleteById(uuid);
+	ResponseEntity<?> excluirItemVenda(@PathVariable("uuid") UUID uuid) {
+		ItemVenda existente = repository.findById(uuid)
+			.orElseThrow(() -> new IllegalArgumentException("Entidade não encontrada"));
+		repository.delete(existente);
+		return ResponseEntity.noContent().build();
 	}
 }
