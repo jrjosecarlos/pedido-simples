753e27c2961460e483c3da878b7ef4d88ca97e03
José Carlos da Conceição Junior <jr_josecarlos@yahoo.com.br>
2020-11-29 21:23:30 -0300

    feature/cadastro-pedido: implementada paginação na listagem de Itens de venda

--- src/main/java/br/org/casa/pedidosimples/controller/ItemVendaController.java
+++ src/main/java/br/org/casa/pedidosimples/controller/ItemVendaController.java
@@ -3,10 +3,11 @@
  */
 package br.org.casa.pedidosimples.controller;
 
-import java.util.List;
 import java.util.UUID;
 import java.util.function.Supplier;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,8 +43,8 @@ public class ItemVendaController {
 	}
 
 	@GetMapping("/itens-venda")
-	ResponseEntity<List<ItemVenda>> listarTodosItensVenda() {
-		return ResponseEntity.ok(repository.findAll());
+	ResponseEntity<Page<ItemVenda>> listarTodosItensVenda(Pageable pageable) {
+		return ResponseEntity.ok(repository.findAll(pageable));
 	}
 
 	@GetMapping("/item-venda/{uuid}")
