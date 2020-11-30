af3007424ac914431a818a12dd83303c3ab1285f
José Carlos da Conceição Junior <jr_josecarlos@yahoo.com.br>
2020-11-30 00:26:44 -0300

    feature/cadastro-pedido: adicionada enumeração em ItemVenda que define os parâmetros de busca para essa classe

--- src/main/java/br/org/casa/pedidosimples/model/ItemVenda.java
+++ src/main/java/br/org/casa/pedidosimples/model/ItemVenda.java
@@ -15,6 +15,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import br.org.casa.pedidosimples.model.enumeration.TipoItemVenda;
+import br.org.casa.pedidosimples.util.EnumUtil;
 
 /**
  * Representa um item que pode ser vendido, ou mais especificamente que pode ser incluso em
@@ -119,6 +120,64 @@ public class ItemVenda extends BaseEntity {
 	 */
 	public void setAtivo(Boolean ativo) {
 		this.ativo = ativo;
+	}
+
+	public enum ParametroBuscaItemVenda {
+		NOME("nome"),
+
+		VALOR_MINIMO("valorMinimo"),
+
+		VALOR_MAXIMO("valorMaximo"),
+
+		TIPO("tipo"),
+
+		ATIVO("ativo");
+
+		private String nomeParametro;
+
+		private ParametroBuscaItemVenda(String nomeParametro) {
+			this.nomeParametro = nomeParametro;
+		}
+
+		/**
+		 * Retorna o valor atual do campo nomeParametro.
+		 *
+		 * @return valor de nomeParametro
+		 */
+		public String getNomeParametro() {
+			return nomeParametro;
+		}
+
+		/**
+		 * Verifica se o nomeParametro informado corresponde a algum dos elementos
+		 * deste enum.
+		 *
+		 * @param nomeParametro o nome do parâmetro a se buscar
+		 * @return {@code true}, se corresponder a algum elemento do enum, {@code false}
+		 * caso contrário
+		 */
+		public static boolean isParametroBuscaVenda(String nomeParametro) {
+			try {
+				ParametroBuscaItemVenda.fromValor(nomeParametro);
+				return true;
+			} catch (IllegalArgumentException e) {
+				return false;
+			}
+		}
+
+		/**
+		 * Converte um valor qualquer no elemento de {@link ParametroBuscaItemVenda} que o possui
+		 * como nomeParametro.
+		 *
+		 * @param valor o valor a ser convertido
+		 * @return o elemento correspondente do enum
+		 * @throws IllegalArgumentException se não há nenhum elemento do enum com nomeParametro igual
+		 * ao valor informado
+		 * @throws NullPointerException se valor for {@code null}.
+		 */
+		public static ParametroBuscaItemVenda fromValor(String valor) {
+			return EnumUtil.enumFromValue(ParametroBuscaItemVenda.class, valor, ParametroBuscaItemVenda::getNomeParametro);
+		}
 	}
 
 }
