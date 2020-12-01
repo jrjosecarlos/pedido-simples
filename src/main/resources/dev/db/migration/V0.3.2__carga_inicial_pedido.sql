082b8f1c7677263e002c9a1694d1690b6aba2141
José Carlos da Conceição Junior <jr_josecarlos@yahoo.com.br>
2020-12-01 17:10:55 -0300

    feature/cadastro-pedido: adicionada segunda migração de desenvolvimento, com dados iniciais de Pedido

---
+++ src/main/resources/dev/db/migration/V0.3.2__carga_inicial_pedido.sql
@@ -0,0 +1,4 @@
+insert into pedido_simples.pedido (id_pedido, codigo, fator_desconto, situacao) values
+('dbbd2adc-3bda-45d7-bf47-172dac89ec3c', '00000001', 0.00, 'A'),
+('bb046b97-7393-483e-a87a-65328e2b0b77', 'aejh7245', 0.50, 'F'),
+('5bf303ed-8104-44d5-a28d-191c679465cd', 'pedi-072', 1.00, 'A')
