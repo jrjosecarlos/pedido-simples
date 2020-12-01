f3a466c0988ca8c9bcc526973a18cd525023844c
José Carlos da Conceição Junior <jr_josecarlos@yahoo.com.br>
2020-12-01 18:14:49 -0300

    feature/cadastro-pedido: adicionado repositório para ItemPedido

---
+++ src/main/java/br/org/casa/pedidosimples/repository/ItemPedidoRepository.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package br.org.casa.pedidosimples.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import br.org.casa.pedidosimples.model.ItemPedido;
+
+/**
+ * Definição de repositório para {@link ItemPedido}.
+ *
+ * @author jrjosecarlos
+ *
+ */
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, UUID>, QuerydslPredicateExecutor<ItemPedido> {
+
+}
