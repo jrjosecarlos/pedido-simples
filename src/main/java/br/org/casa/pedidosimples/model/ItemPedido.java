d0c4794d08795e54477fb6df49cc9abc4d22cf71
José Carlos da Conceição Junior <jr_josecarlos@yahoo.com.br>
2020-12-01 22:24:11 -0300

    feature/cadastro-pedido: implementado método para cálculo do valor do ItemPedido, levando em consiração o valor do ItemVenda, o fator de desconto do tipo dele e o fator de desconto do Pedido associado.
    Diminuída visibilidade do método setValor, de modo a direcionar o preenchimento deste campo apenas via método de cálculo de valor

--- src/main/java/br/org/casa/pedidosimples/model/ItemPedido.java
+++ src/main/java/br/org/casa/pedidosimples/model/ItemPedido.java
@@ -19,7 +19,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.querydsl.core.types.dsl.BooleanExpression;
 
 import br.org.casa.pedidosimples.exception.ParametroBuscaParseException;
+import br.org.casa.pedidosimples.model.enumeration.TipoItemVenda;
 import br.org.casa.pedidosimples.util.EnumUtil;
+import br.org.casa.pedidosimples.util.BigDecimalUtil;
 
 /**
  * Entidade que representa os {@link ItemVenda} associados a um {@link Pedido}
@@ -71,7 +73,7 @@ public class ItemPedido extends BaseEntity {
 	 *
 	 * @param valor o novo valor de valor
 	 */
-	public void setValor(BigDecimal valor) {
+	void setValor(BigDecimal valor) {
 		this.valor = valor;
 	}
 
@@ -109,6 +111,33 @@ public class ItemPedido extends BaseEntity {
 	 */
 	public void setItemVenda(ItemVenda itemVenda) {
 		this.itemVenda = itemVenda;
+	}
+
+	/**
+	 * Atualizar o valor deste ItemPedido. O valor atualizado corresponde ao valor base
+	 * do {@link ItemVenda} associado, aplicando o percentual de desconto do {@link Pedido},
+	 * modificado pelo {@link TipoItemVenda tipo do ItemVenda}, ao qual este item pertence.
+	 *
+	 * <p>Por exemplo, se o ItemVenda é de um tipo com fator de desconto 1.00, possui valor base
+	 * igual a 200.00 e o Pedido tem um desconto de 30%, o valor do ItemPedido será:</p>
+	 *
+	 * <code>200.00 - (200.00 * (0.30 * 1.00)) = 140.00</code>
+	 *
+	 * <p>Já se o ItemVenda é de um tipo com fator de desconto 0.00, possuir valor base igual a
+	 * 150.00 e o pedido tem um desconto de 50%, o valor do ItemPedido será:</p>
+	 *
+	 * <code> 150.00 - (150 * (0.50 * 0.00)) = 150.00</code>
+	 *
+	 * @return o valor atualizado deste ItemPedido.
+	 */
+	public BigDecimal calcularValor() {
+		BigDecimal fatorDescontoEfetivo = this.getPedido().getFatorDesconto()
+				.multiply(this.getItemVenda().getTipo().getFatorDesconto());
+		BigDecimal valorAtualizado = this.getItemVenda().getValorBase()
+				.subtract(this.getItemVenda().getValorBase().multiply(fatorDescontoEfetivo));
+
+		this.setValor(BigDecimalUtil.setEscalaPadrao(valorAtualizado));
+		return this.valor;
 	}
 
 	public enum ParametroBuscaItemPedido {
